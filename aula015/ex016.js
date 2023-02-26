07e9a9e07347abefcd8c3ed842af6d8477ae57db
Manuela Gomes <manudeveloper82@gmail.com>
2023-02-25 22:01:10 -0300

    contagem

---
+++ aula015/ex016.js
@@ -0,0 +1,49 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c < 3)
+*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+----------------------------//------------------------
+*/
+
+
+/*
+var c=1
+while (c<=10) {
+    console.log(`passo ${c}`)
+    c++
+}
+----------------------//-------------
+
+for( var c=1; c<=10; c++){
+    console.log(`Passo ${c}`)
+}
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c <= 3; c++) {
+    console.log(c)
+}
+console.log('Fim!')
